ea27320facde53e29d1e780d41c02a9e2d969ca7
SRodriguezIITH <bm23btech11023@iith.ac.in>
2023-12-27 10:08:09 +0530

    Initial commit

---
+++ app/src/main/java/com/learnapp/firstxmlapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.learnapp.firstxmlapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
